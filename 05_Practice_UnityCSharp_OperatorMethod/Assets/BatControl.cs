70fd3af92553fa95c61026b2d6be4151f83dba02
xiaoshane0408 <63327406+xiaoshane0408@users.noreply.github.com>
2020-04-28 16:34:37 +0800

    practice建檔

---
+++ 05_Practice_UnityCSharp_OperatorMethod/Assets/BatControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BatControl : MonoBehaviour
+{
+    [Header("血量")]
+    public int blood;
+    [Header("攻擊力")]
+    public int atk;
+    [Header("治癒量")]
+    public int cure;
+    [Header("顯示訊息")]
+    public Text message;
+
+    public void Attack()
+    {
+
+    }
+}
