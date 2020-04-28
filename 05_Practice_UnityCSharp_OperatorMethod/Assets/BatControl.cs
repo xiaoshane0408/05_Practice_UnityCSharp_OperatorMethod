4f6a61a374c002b007f2a84aaa524aa93d81ba84
xiaoshane0408 <63327406+xiaoshane0408@users.noreply.github.com>
2020-04-28 20:05:28 +0800

    done

--- 05_Practice_UnityCSharp_OperatorMethod/Assets/BatControl.cs
+++ 05_Practice_UnityCSharp_OperatorMethod/Assets/BatControl.cs
@@ -4,16 +4,41 @@ using UnityEngine.UI;
 public class BatControl : MonoBehaviour
 {
     [Header("血量")]
-    public int blood;
+    public int blood = 200;
     [Header("攻擊力")]
-    public int atk;
+    public int atk = 30;
     [Header("治癒量")]
-    public int cure;
+    public int cure = 10;
     [Header("顯示訊息")]
     public Text message;
+    public SlimeControl slime;
 
+    /// <summary>
+    /// 攻擊
+    /// </summary>
     public void Attack()
     {
+        slime.Injure(atk);
+    }
 
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="inj">收到史萊姆攻擊力</param>
+    public void Injure(int inj)
+    {
+        blood = blood - inj;
+        message.text = gameObject.name + " - 受到傷害：" + inj + "\n"
+                     + gameObject.name + " - 血量剩下：" + blood;
+    }
+
+    /// <summary>
+    /// 治癒
+    /// </summary>
+    public void Cure()
+    {
+        blood += cure;
+        message.text = gameObject.name + " - 收到治癒：" + cure + "\n"
+                     + gameObject.name + " - 血量剩下：" + blood;
     }
 }
--- 05_Practice_UnityCSharp_OperatorMethod/Assets/SlimeControl.cs
+++ 05_Practice_UnityCSharp_OperatorMethod/Assets/SlimeControl.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SlimeControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("血量")]
+    public int blood = 300;
+    [Header("攻擊力")]
+    public int atk = 10;
+    [Header("治癒量")]
+    public int cure = 20;
+    [Header("顯示訊息")]
+    public Text message;
+    public BatControl bat;
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    public void Attack()
     {
-        
+        bat.Injure(atk);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="inj">收到蝙蝠攻擊力</param>
+    public void Injure(int inj)
     {
-        
+        blood = blood - inj;
+        message.text = gameObject.name + " - 受到傷害：" + inj + "\n"
+                     + gameObject.name + " - 血量剩下：" + blood;
+    }
+
+    /// <summary>
+    /// 治癒
+    /// </summary>
+    public void Cure()
+    {
+        blood += cure;
+        message.text = gameObject.name + " - 收到治癒：" + cure + "\n"
+                     + gameObject.name + " - 血量剩下：" + blood;
     }
 }
